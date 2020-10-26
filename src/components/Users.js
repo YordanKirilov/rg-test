b7e2641195575d38f3880e9e9b8ba6ca4852c5f7
Yordan Kirilov <jordan.kirilov@gmail.com>
2020-10-26 09:26:14 +0200

    Adds static user avatar in case of broken avatar urls

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { stripScripts, stripTags } from '../utils/textUtils';
 import styled from 'styled-components';
+import userAvatarPlaceholder from '../assets/avatardefault.png';
 
 const UserList = styled.ul`
   padding: 0;
   > li {
     display: grid;
-    grid-template-columns: 64px 1fr 3fr;
+    grid-template-columns: 65px 1fr 3fr;
+    grid-template-rows: 65px;
     grid-column-gap: 8px;
     list-style: none;
     text-align: left;
@@ -20,6 +22,10 @@ const UserList = styled.ul`
     .user__avatar {
       grid-area: 1 / 1 / 3 / 2;
       border-right: 1px solid #eee;
+      img {
+        display: block;
+        width: 100%;
+      }
     }
     .user__name {
       grid-area: 1 / 2 / 3 / 3;
@@ -27,13 +33,20 @@ const UserList = styled.ul`
       > h4 {
         margin: 0 0 16px 0;
       }
+      > p {
+        margin: 0;
+      }
     }
     .user__bio {
       grid-area: 1 / 3 / 3 / 4;
-      padding-bottom: 16px;
+      padding-bottom: 16;
     }
   }
 `;
+
+const handleImageError = (e) => {
+  e.target.src = userAvatarPlaceholder;
+};
 
 const Users = ({ users = [], loading }) => {
   if (loading) {
@@ -44,7 +57,11 @@ const Users = ({ users = [], loading }) => {
       {users.map((user) => (
         <li key={user.uuid}>
           <div className="user__avatar">
-            <img src={user.avatar} alt={user.name.substring(0, 1)} />
+            <img
+              src={user.avatar}
+              alt={user.name.substring(0, 1)}
+              onError={handleImageError}
+            />
           </div>
           <div className="user__name">
             <h4>{user.name}</h4> <p>{user.company}</p>
